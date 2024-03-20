416ef4ed7cbb87b2f109b1ea0a5276a27f08cd38
fishok11 <svllsd811@gmail.com>
2024-03-20 10:04:32 +0300

    i tailwind

--- src/app/mainSlice.ts
+++ src/app/mainSlice.ts
@@ -2,49 +2,43 @@ import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { RootState, AppThunk } from './store';
 
 type InitialState = {
-
-}
+  isLoading: boolean;
+};
 
 const initialState: InitialState = {
- 
+  isLoading: false,
 };
 
 // export const incrementAsync = createAsyncThunk(
 //   'main/fetchCount',
 //   async (amount: number) => {
-  
+
 //   }
 // );
 
 export const mainSlice = createSlice({
   name: 'main',
   initialState,
- 
-  reducers: {
 
-  },
- 
+  reducers: {},
+
   extraReducers: (builder) => {
-    builder
-      // .addCase(incrementAsync.pending, (state) => {
-      //   state.status = 'loading';
-      // })
-      // .addCase(incrementAsync.fulfilled, (state, action) => {
-      //   state.status = 'idle';
-      //   state.value += action.payload;
-      // })
-      // .addCase(incrementAsync.rejected, (state) => {
-      //   state.status = 'failed';
-      // });
+    builder;
+    // .addCase(incrementAsync.pending, (state) => {
+    //   state.status = 'loading';
+    // })
+    // .addCase(incrementAsync.fulfilled, (state, action) => {
+    //   state.status = 'idle';
+    //   state.value += action.payload;
+    // })
+    // .addCase(incrementAsync.rejected, (state) => {
+    //   state.status = 'failed';
+    // });
   },
 });
 
-export const { } = mainSlice.actions;
-
+// export const {} = mainSlice.actions;
 
 export const stateMainSlice = (state: RootState) => state.main;
-
-
-
 
 export default mainSlice.reducer;
